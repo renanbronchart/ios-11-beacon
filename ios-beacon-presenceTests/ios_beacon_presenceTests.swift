5a80f36bde49a185146c82aad1e86d216767cfff
renanbronchart <renan.bronchart@gmail.com>
2018-04-26 21:38:16 +0200

    Initial Commit

---
+++ ios-beacon-presenceTests/ios_beacon_presenceTests.swift
@@ -0,0 +1,36 @@
+//
+//  ios_beacon_presenceTests.swift
+//  ios-beacon-presenceTests
+//
+//  Created by Renan Bronchart on 26/04/2018.
+//  Copyright © 2018 Renan Bronchart. All rights reserved.
+//
+
+import XCTest
+@testable import ios_beacon_presence
+
+class ios_beacon_presenceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
