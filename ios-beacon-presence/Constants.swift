77869da45bd2fee73c1476aca8f2f3fe61dbe3a3
renanbronchart <renan.bronchart@gmail.com>
2018-04-27 13:41:48 +0200

    [Feat:constants] Add url constants

---
+++ ios-beacon-presence/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  constants.swift
+//  ios-beacon-presence
+//
+//  Created by Renan Bronchart on 27/04/2018.
+//  Copyright © 2018 Renan Bronchart. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    let BASE_URL = "https://d1a32b63.ngrok.io/"
+}
